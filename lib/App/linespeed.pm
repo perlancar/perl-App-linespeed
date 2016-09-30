c1dc00fff596c65fda9ca0cac9e12f12b504cdc5
perlancar (@pc-office) <perlancar@gmail.com>
2016-09-30 19:57:16 +0700

    Initial commit

---
+++ lib/App/linespeed.pm
@@ -0,0 +1,18 @@
+package App::linespeed;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Calculate how fast input lines are coming in
+
+=head1 SYNOPSIS
+
+See the command-line script L<linespeed>.
+
+
+=head1 SEE ALSO
+
+L<numspeed> from L<App::numspeed>
+
+=cut
